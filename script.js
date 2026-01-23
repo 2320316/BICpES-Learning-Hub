60faa2d13fd1dd31747adca3e9d195f6d326df16
Kielot <kielmanalocarandang@gmail.com>
2026-01-23 23:21:18 +0800

    Test button JS

---
+++ script.js
@@ -0,0 +1,21 @@
+const loginButton = document.getElementById("login");
+const cancelButton = document.getElementById("cancel");
+
+// login button
+loginButton.addEventListener("click", () => {
+    const username = document.getElementById("Username").value;
+    const password = document.getElementById("Password").value;
+
+    // Simpleng validation example
+    if (username === "admin" && password === "1234") {
+        alert("Login successful!");
+        // Redirect to dashboard/homepage
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid username or password.");
+    }
+});
+
+cancelButton.addEventListener("click", () => {
+    window.location.href="index.html";
+});
